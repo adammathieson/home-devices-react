d619029b9e083d7b2dfa1caa0ccf3423db1db0d4
Adam Mathieson <mathieson1222@gmail.com>
2020-07-31 23:10:14 -0700

    adds the initial svgfiles and sets up the basic file and app stucture

---
+++ src/components/SceneContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import SceneOne from './scene1/SceneOne'
+
+const SceneContainer = () => {
+    return (
+        <div className='container'>
+
+            <SceneOne />
+            {/* <Scene2 /> */}
+            {/* <Scene3 /> */}
+            {/* <Scene4 /> */}
+            {/* <Scene5 /> */}
+        </div>
+    )
+}
+
+export default SceneContainer
---
+++ src/components/scene1/SceneOne.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import scene1 from './scene1.svg'
+
+
+
+const SceneOne = () => {
+    return (
+        <div>
+            <img src={scene1} alt="A delivery person carries a package to the front door of a house"></img>
+        </div>
+    )
+}
+
+export default SceneOne
+
+
+

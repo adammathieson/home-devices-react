2cb4423a7bfce2f36cf146e9faf42b8522a8f015
Adam Mathieson <mathieson1222@gmail.com>
2020-08-01 18:51:13 -0700

    Scene5 added and rendering

--- src/scenes/SceneContainer.js
+++ src/scenes/SceneContainer.js
@@ -3,16 +3,17 @@ import Scene1 from './Scene1'
 import Scene2 from './Scene2'
 import Scene3 from './Scene3'
 import Scene4 from './Scene4'
+import Scene5 from './Scene5'
 
 
 const SceneContainer = () => {
     return (
         <div className='container'>
 
-            {/* <Scene1 /> */}
+            <Scene1 />
             {/* <Scene2 /> */}
             {/* <Scene3 /> */}
-            <Scene4 />
+            {/* <Scene4 /> */}
             {/* <Scene5 /> */}
         </div>
     )

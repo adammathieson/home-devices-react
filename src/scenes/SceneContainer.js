fe8a70b1e59a1b9aa0ea7e53804af31c472b1064
Adam Mathieson <mathieson1222@gmail.com>
2020-08-06 00:00:55 -0700

    cleanup

--- src/scenes/SceneContainer.js
+++ src/scenes/SceneContainer.js
@@ -1,26 +1,21 @@
 import React from 'react'
 import { useStore } from '../Store'
+
 import Scene1 from './Scene1'
 import Scene2 from './Scene2'
 import Scene3 from './Scene3'
 import Scene4 from './Scene4'
 import Scene5 from './Scene5'
 
+// Render the current scene by state -> updates via onComplete of each scene animation
 const sceneArr = [<Scene1/>, <Scene2/>, <Scene3/>, <Scene4/>, <Scene5/>]
 
-const SceneSelector = () => {
-    const scene = useStore(state => state.scene)
-    
-    return sceneArr[scene]
-
-}
-
 const SceneContainer = () => {
-
+    const scene = useStore(state => state.scene)
 
     return (
         <div className='container'>
-                <SceneSelector />
+                {sceneArr[scene]}
         </div>
     )
 }

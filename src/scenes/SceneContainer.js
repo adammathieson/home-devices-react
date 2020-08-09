71ccbb384170500e8255ffb99faca0704f372934
Adam Mathieson <mathieson1222@gmail.com>
2020-08-09 01:13:20 -0700

    scene3 svg and animation updated

--- src/scenes/SceneContainer.js
+++ src/scenes/SceneContainer.js
@@ -16,7 +16,7 @@ const SceneContainer = () => {
     return (
         <div className='container'>
                 {/* {sceneArr[scene]} */}
-                <Scene2/>
+                <Scene3/>
         </div>
     )
 }

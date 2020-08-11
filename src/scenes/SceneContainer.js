28ade9ac973a7a2662a144b20817280b689b702a
Adam Mathieson <mathieson1222@gmail.com>
2020-08-10 19:16:13 -0700

    adjusts position and animations of scene2

--- src/scenes/SceneContainer.js
+++ src/scenes/SceneContainer.js
@@ -16,7 +16,7 @@ const SceneContainer = () => {
     return (
         <div className='container'>
                 {/* {sceneArr[scene]} */}
-                <Scene3/>
+                <Scene2/>
         </div>
     )
 }

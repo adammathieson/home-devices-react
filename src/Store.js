d29ae5eaf60629b92f4e227bb139886c03875f4d
Adam Mathieson <mathieson1222@gmail.com>
2020-08-10 18:13:09 -0700

    adds replay button, fn, and animation

--- src/Store.js
+++ src/Store.js
@@ -4,6 +4,6 @@ const maxScene = 4
 export const [useStore] = create(set => ({
     scene: 0,
     nextScene: () => set(state => ({ 
-        scene: state.scene < maxScene ? state.scene +1 : 1
+        scene: state.scene < maxScene ? state.scene +1 : 0
     }))
 }))
